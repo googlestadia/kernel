fe9bee2bfc519dff6ea23b280a53e6e57746a946
Christian König <christian.koenig@amd.com>
2018-03-23 16:33:57 +0100

    drm/amdgpu: add checks if DMA-buf P2P is supported
    Check if we can do peer2peer on the PCIe bus.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Acked-by: Sumit Semwal <sumit.semwal@linaro.org>
    Link: https://patchwork.freedesktop.org/patch/359294
    Change-Id: I16287ce6eadc7a86e25e51c3f9a951797416acb7

--- drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_dma_buf.c
@@ -38,6 +38,7 @@
 #include <drm/amdgpu_drm.h>
 #include <linux/dma-buf.h>
 #include <linux/dma-fence-array.h>
+#include <linux/pci-p2pdma.h>
 
 #if !defined(HAVE_DMA_BUF_OPS_DYNAMIC_MAPPING)
 /**
@@ -294,6 +295,9 @@ static int amdgpu_dma_buf_attach(struct dma_buf *dmabuf,
 	struct amdgpu_bo *bo = gem_to_amdgpu_bo(obj);
 	struct amdgpu_device *adev = amdgpu_ttm_adev(bo->tbo.bdev);
 	int r;
+
+	if (pci_p2pdma_distance_many(adev->pdev, &attach->dev, 1, true) < 0)
+		attach->peer2peer = false;
 
 	if (attach->dev->driver == adev->dev->driver)
 		return 0;
