298219aeb8cd5b6f74c95e31f57403c3213af8b4
Jean-Francois Thibert <jfthibert@google.com>
2019-06-05 11:53:05 -0400

    google: Add Stadia build script files
    Change-Id: I9408e1644e861154251ab21129f507c532cb45d3

---
+++ kokoro/ubuntu/gamelet_kernel/gen_buildstamp_vars.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+"""Generate a file from Bazel workspace status files using a template.
+
+See
+https://docs.bazel.build/versions/master/user-manual.html#flag--workspace_status_command
+"""
+
+import argparse
+import errno
+import io
+import os
+import re
+import time
+
+KOKORO_RELEASE_TRAIN_RE = re.compile(r"(\d{4}\.\d+)/release")
+RAPID_RC_NUMBER_RE = re.compile(r"_RC(\d+)$")
+RAPID_RELEASE_TRAIN_RE = re.compile(r"(\d{4}\.\d+)_RC\d+$")
+SCM_BRANCH_RELEASE_TRAIN_RE = re.compile(r"^release/gamelet/(\d{4}\.\d+)")
+
+
+def main():
+  """main."""
+  parser = argparse.ArgumentParser(
+      "Generates a version header from Bazel workspace status files.")
+  parser.add_argument("--output", help="Output file.")
+  parser.add_argument(
+      "--template",
+      help=
+      "Template file for the output file using Python's format string syntax. Use 'vars' as the token."
+  )
+  parser.add_argument(
+      "--var_format",
+      help=
+      "Format string for a buildstamp variable using Python's format string syntax. Use 'k' and 'v' tokens."
+  )
+  parser.add_argument(
+      "status_files",
+      metavar='<path>',
+      nargs='+',
+      help="One or more files containing buildstamp status lines."
+  )
+  args = parser.parse_args()
+  output_dir = os.path.dirname(args.output)
+  try:
+    os.makedirs(output_dir, mode=0o755)
+  except OSError as e:
+    if e.errno != errno.EEXIST:
+      raise
+  with io.open(args.template) as f:
+    template = f.read()
+  status = ''
+  for status_file in args.status_files:
+    with io.open(status_file) as f:
+      status += f.read()
+  vars = {
+      "BUILD_EMBED_LABEL": "",
+      "BUILD_HOST": "",
+      "BUILD_TIMESTAMP": "0",
+      "BUILD_USER": "",
+      "STABLE_BUILD_SCM_BRANCH": "",
+      "STABLE_BUILD_SCM_REVISION": "",
+      "STABLE_BUILD_SCM_STATUS": "",
+      "STABLE_KOKORO_BUILD_ID": "",
+      "STABLE_KOKORO_BUILD_NUMBER": "0",
+      "STABLE_KOKORO_JOB_NAME": "",
+      "STABLE_KOKORO_JOB_TYPE": "",
+      "STABLE_RAPID_CANDIDATE_NAME": "",
+      "STABLE_RELEASE_CANDIDATE_NUMBER": "0",
+      "STABLE_RELEASE_TRAIN": "0000.0",
+  }
+  for l in status.splitlines():
+    k, v = l.split(" ", 1)
+    vars[k] = v
+  # Extract information from STABLE_KOKORO_JOB_NAME.
+  if vars["STABLE_KOKORO_JOB_NAME"]:
+    m = KOKORO_RELEASE_TRAIN_RE.search(vars["STABLE_KOKORO_JOB_NAME"])
+    if m:
+      vars["STABLE_RELEASE_TRAIN"] = m.group(1)
+  # Extract information from STABLE_RAPID_CANDIDATE_NAME.
+  if vars["STABLE_RAPID_CANDIDATE_NAME"]:
+    m = RAPID_RC_NUMBER_RE.search(vars["STABLE_RAPID_CANDIDATE_NAME"])
+    if m:
+      vars["STABLE_RELEASE_CANDIDATE_NUMBER"] = m.group(1)
+    m = RAPID_RELEASE_TRAIN_RE.search(vars["STABLE_RAPID_CANDIDATE_NAME"])
+    if m:
+      vars["STABLE_RELEASE_TRAIN"] = m.group(1)
+  # Extract information from STABLE_BUILD_SCM_BRANCH.
+  if vars["STABLE_BUILD_SCM_BRANCH"]:
+    m = SCM_BRANCH_RELEASE_TRAIN_RE.search(vars["STABLE_BUILD_SCM_BRANCH"])
+    if m:
+      vars["STABLE_RELEASE_TRAIN"] = m.group(1)
+  # Format BUILD_TIMESTAMP and store as BUILD_TIMESTAMP_FMT.
+  vars["BUILD_TIMESTAMP_FMT"] = time.strftime(
+      "%c %Z", time.localtime(int(vars["BUILD_TIMESTAMP"])))
+  # Process the template and output the result.
+  print("INFO: Formatting buildstamp variables with format: " + args.var_format)
+  vars_str = "\n".join(
+      args.var_format.format(k, v) for k, v in sorted(vars.items()))
+  with io.open(args.output, "w+", encoding="utf-8") as f:
+    f.write(template.format(vars=vars_str, **vars))
+
+
+if __name__ == "__main__":
+  main()
---
+++ stadia/initramfs/root-image/scripts/overlay.sh
@@ -0,0 +1,189 @@
+#!/bin/sh
+
+# Use overlayfs to stack a read-only root filesystem and squashfs filesystems
+# (used to inject MPMs). The writable layer (aka the upperdir) is placed on the
+# first writable block device with LABEL "OVERLAYRWFS", or on the /run tmpfs if
+# there is no such block device.
+mount_root_overlayfs() {
+	# Create a container directory for the overlayfs. Under this container,
+	# the boot fs is mounted as "boot", the root fs is mounted as "root",
+	# each squashfs block device is mounted as "s<index>", the read-write
+	# directory is mounted or created as "rw" (see above), and the workdir
+	# is created as "work".
+	local ofs_dir="/run/ofs"
+	mkdir -p "${ofs_dir}"
+
+	local boot_mnt="${ofs_dir}/boot"
+	local root_mnt="${ofs_dir}/root"
+	local mofi_mnt="${ofs_dir}/mofi"
+	local rwfs_mnt="${ofs_dir}/rwfs"
+
+	local squashfs_layer_index=0
+	local upperdir="${ofs_dir}/rw"
+	local workdir="${ofs_dir}/work"
+	local lowerdir=""
+
+	local bootfs_dev=""
+	local bootfs_fstype=""
+	local rootfs_dev=""
+	local rootfs_devid=""
+	local rwfs_dev=""
+	local rwfs_fstype=""
+	local ggprootfs_dev=""
+	local ggprootfs_fstype=""
+
+	# If root=UUID=<> or root=LABEL=<> was specified in cmdline, set
+	# $rootfs_devid to just <> to resolve $rootfs_dev to an actual device
+	# node.
+	#
+	# If root=auto was specified in cmdline, leave $rootfs_devid="" and
+	# $rootfs_dev="". If $ggprootfs_dev is resolved, then $rootfs_dev will
+	# be set to ggprootfs_dev. If $ggprootfs_dev is not resolved, there will
+	# be no rootfs inserted in the overlayfs.
+	#
+	# Otherwise, if root=<dev>, set $rootfs_dev=<dev>.
+	case "${cmdline_root}" in
+		UUID=*)
+			eval "${cmdline_root}"
+			rootfs_devid="${UUID}"
+			UUID=
+			;;
+		LABEL=*)
+			eval "${cmdline_root}"
+			rootfs_devid="${LABEL}"
+			LABEL=
+			;;
+		auto)
+			;;
+		*)
+			rootfs_dev="${cmdline_root}"
+			;;
+	esac
+
+	# Iterate over `lsblk` lines to resolve boot, root, rw, and ggproot fs.
+	# A sample line with lsblk -P:
+	#    NAME="sda3" FSTYPE="ext2" LABEL="" UUID="" RO="0"
+	while read -r line; do
+		# Evaluate the line with the shell.
+		eval "${line}"
+		blk_dev="/dev/${NAME}"
+
+		# Set $bootfs_dev if it is empty and the device LABEL is
+		# "GGPBOOTFS". Also set $bootfs_fstype to FSTYTPE.
+		if [ -z "${bootfs_dev}" ] && [ "GGPBOOTFS" = "${LABEL}" ]; then
+			bootfs_dev="${blk_dev}"
+			bootfs_fstype="${FSTYPE}"
+		fi
+
+		# Resolve $rootfs_dev if it is empty and if $UUID or $LABEL
+		# match $rootfs_devid.
+		if [ -z "${rootfs_dev}" ] &&  [ -n "${UUID}" ] && [ "${rootfs_devid}" = "${UUID}" ]; then
+			rootfs_dev="${blk_dev}"
+		fi
+		if [ -z "${rootfs_dev}" ] &&  [ -n "${LABEL}" ] && [ "${rootfs_devid}" = "${LABEL}" ]; then
+			rootfs_dev="${blk_dev}"
+		fi
+
+		# Set $rootfstype if it is blank and this is the root device.
+		if [ "${blk_dev}" = "${rootfs_dev}" ] && [ -z "${rootfstype}" ]; then
+			rootfstype="${FSTYPE}"
+		fi
+
+		# Set $ggprootfs_dev if it is empty and the device LABEL is
+		# "GGPROOTFS". Also set $ggprootfs_fstype to FSTYTPE.
+		if [ -z "${ggprootfs_dev}" ] && [ "GGPROOTFS" = "${LABEL}" ]; then
+			ggprootfs_dev="${blk_dev}"
+			ggprootfs_fstype="${FSTYPE}"
+		fi
+
+		# Set $rwfs_dev if it is empty and the device LABEL is
+		# "OVERLAYRWFS" and RO == 0. Also set $rwfs_fstype to FSTYTPE.
+		if [ -z "${rwfs_dev}" ] && [ "OVERLAYRWFS" = "${LABEL}" ] && [ "0" = "${RO}" ]; then
+			rwfs_dev="${blk_dev}"
+			rwfs_fstype="${FSTYPE}"
+		fi
+	done <<EOT
+$(lsblk -P -o NAME,FSTYPE,LABEL,UUID,RO)
+EOT
+
+	# If $rootfs_dev is empty and $ggprootfs_dev is not, set
+	# $rootfs_dev=$ggprootfs_dev. Do the same for rootfstype.
+	if [ -z "${rootfs_dev}" ] && [ -n "${ggprootfs_dev}" ]; then
+		rootfs_dev="${ggprootfs_dev}"
+		rootfstype="${ggprootfs_fstype}"
+	fi
+
+	# Iterate over `lsblk` lines to build the overlay's squashfs lowerdir.
+	# A sample line with lsblk -P:
+	#    NAME="sda3" FSTYPE="ext2" LABEL="" UUID="" RO="0"
+	while read -r line; do
+		# Evaluate the line with the shell.
+		eval "${line}"
+		blk_dev="/dev/${NAME}"
+
+		# If the block device is a squashfs, and it is not the root
+		# device, mount it read-only and prepend its mount path to
+		# `lowerdir`. overlayfs stacks layers from right to left
+		# (leftmost is top).
+		if [ "${FSTYPE}" = "squashfs" ] && [ "${blk_dev}" != "${rootfs_dev}" ]; then
+			local squash_mnt="${ofs_dir}/s${squashfs_layer_index}"
+			mkdir "${squash_mnt}"
+			mount -t squashfs -r "${blk_dev}" "${squash_mnt}" \
+				|| return 1
+			squashfs_layer_index=$((${squashfs_layer_index} + 1))
+			lowerdir="${squash_mnt}:${lowerdir}"
+		fi
+	done <<EOT
+$(lsblk -P -o NAME,FSTYPE,LABEL,UUID,RO)
+EOT
+
+	# If a boot fs was found, mount it read-only and append its mount path
+	# to `lowerdir`. overlayfs stacks layers from right to left (leftmost is
+	# top). If `lowerdir` is not empty, it will contain a suffix ':'.
+	if [ -n "${bootfs_dev}" ]; then
+		mkdir "${boot_mnt}"
+		mount -t "${bootfs_fstype}" -r "${bootfs_dev}" "${boot_mnt}" \
+			|| return 1
+		lowerdir="${lowerdir}${boot_mnt}:"
+	fi
+
+	# If a modules-and-firmware squashfs was bundled with the initramfs,
+	# mount it and append its mount path to `lowerdir`. overlayfs stacks
+	# layers from right to left (leftmost is top). If `lowerdir` is not
+	# empty, it will contain a suffix ':'.
+	if [ -f "${mofidev}" ]; then
+		mkdir "${mofi_mnt}"
+		mount -t squashfs -r "${mofidev}" "${mofi_mnt}" || return 1
+		lowerdir="${lowerdir}${mofi_mnt}:"
+	fi
+
+	# If a root fs was specified or found, mount it read-only and append its
+	# mount path to `lowerdir`. overlayfs stacks layers from right to left
+	# (leftmost is top). If `lowerdir` is not empty, it will contain a
+	# suffix ':'.
+	if [ -n "${rootfs_dev}" ]; then
+		mkdir "${root_mnt}"
+		mount -t "${rootfstype}" "${rootro}" -o "${rootflags}" \
+			"${rootfs_dev}" "${root_mnt}" || return 1
+		lowerdir="${lowerdir}${root_mnt}:"
+	fi
+
+	# Finalize `lowerdir` by stripping any trailing ':'.
+	lowerdir=${lowerdir%:}
+
+	# If a overlayfs read-write filesystem was found, mount it and place the
+	# overlayfs upperdir and workdir on it.
+	if [ -n "${rwfs_dev}" ]; then
+		mkdir "${rwfs_mnt}"
+		mount -t "${rwfs_fstype}" "${rwfs_dev}" "${rwfs_mnt}" \
+			|| return 1
+		upperdir="${rwfs_mnt}/rw"
+		workdir="${rwfs_mnt}/work"
+	fi
+
+	# Mount the overlayfs.
+	mkdir "${upperdir}" "${workdir}"
+	mount -t overlay overlay -o \
+		"lowerdir=${lowerdir},upperdir=${upperdir},workdir=${workdir}" \
+		/root || return 1
+}
