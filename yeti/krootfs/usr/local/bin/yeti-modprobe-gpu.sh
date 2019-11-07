2ba8a9bbafcda76efac45c2da7d41244fd8d5521
Jean-Francois Thibert <jfthibert@google.com>
2019-11-07 10:02:00 -0500

    Bump peermem limit to 192MB
    Bug: 143700516
    Change-Id: Ia50b72a8d402f02f61962188d79817e07b2237ec
    (cherry picked from commit 9d79899e1c3b9ffc18a27088de657bfd09107920)

--- yeti/krootfs/usr/local/bin/yeti-modprobe-gpu.sh
+++ yeti/krootfs/usr/local/bin/yeti-modprobe-gpu.sh
@@ -2,9 +2,9 @@
 TOTAL_RAM=$(awk '$1 == "MemTotal:" { print $2; exit }' < /proc/meminfo)
 GTT_RAM=$((${TOTAL_RAM}*3/(4*1024)))
 # --peermem_size must be large enough to fit all DGMA buffers of maximum frame
-# size (each 4k HDR is ~16 MB, 8k HDR is 62 MB). Sizes larger than 64 MB are
-# subdivided but we need more to work around BO locking issue so set to 128 MB.
-PEER_SIZE=128
+# size (each 4k HDR is ~16 MB, 8k HDR is 62 MB). A game with 3 FB will use 48MB
+# but we need more to work around BO locking issue so set to 192 MB.
+PEER_SIZE=192
 
 modprobe amdgpu gttsize="${GTT_RAM}" sched_jobs=256 sched_hw_submission=8 \
   peermem_size="${PEER_SIZE}" no_evict=1
