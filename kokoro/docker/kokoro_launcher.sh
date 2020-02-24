4535ee4d93ca3c3c64b98a7fe7d27f59290bdfc8
Jean-François Roy <jfroy@google.com>
2020-02-24 09:26:16 -0800

    kokoro: Download the initramfs archive with wget
    Download the initramfs archive from a public GCS bucket to allow for
    open-source builds.
    
    Test: Build with glinux_launcher.sh. Presubmits pass.
    
    Bug: 150214112
    Change-Id: I12afca0e14f3c8e1a4c3245ace13306e76b0fff5

--- kokoro/docker/kokoro_launcher.sh
+++ kokoro/docker/kokoro_launcher.sh
@@ -37,7 +37,7 @@ docker run \
   --env "TMP=/workspace/tmp" \
   --net=host \
   --privileged=true \
-  -t gcr.io/stadia-open-source/build/kernel@sha256:388ff48eea75ffcdd027bf7e7109e21aaae7f857ddd2880aa544f47f26f0c82d \
+  -t gcr.io/stadia-open-source/build/kernel@sha256:476f6e5d2c4f4ef4ec25773d1b1a9bb48cf9d65a1275adad39611f8ed0185e8f \
   /container_tools/fix_permissions.sh --user "$(id -u):$(id -g)" \
   -- \
   /workspace/src/kernel/kokoro/build.sh
