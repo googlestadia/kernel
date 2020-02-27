ac90a2cf94d65a47076ac1e679e84a8f132baec3
Nirmoy Das <nirmoy.das@amd.com>
2020-02-27 13:59:08 +0100

    drm/amdgpu: set compute queue priority at mqd_init
    We were changing compute ring priority while rings were being used
    before every job submission which is not recommended. This patch
    sets compute queue priority at mqd initialization for gfx8, gfx9 and
    gfx10.
    
    Policy: make queue 0 of each pipe as high priority compute queue
    
    High/normal priority compute sched lists are generated from set of high/normal
    priority compute queues. At context creation, entity of compute queue
    get a sched list from high or normal priority depending on ctx->priority
    
    Signed-off-by: Nirmoy Das <nirmoy.das@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Acked-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ring.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ring.h
@@ -223,6 +223,7 @@ struct amdgpu_ring {
 	struct mutex		priority_mutex;
 	/* protected by priority_mutex */
 	int			priority;
+	bool			has_high_prio;
 
 #if defined(CONFIG_DEBUG_FS)
 	struct dentry *ent;
