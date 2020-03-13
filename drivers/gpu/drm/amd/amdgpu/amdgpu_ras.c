1c10aa0ca2eb08a92f3916aa62e51ad4b9ba6990
Yifan Zhang <yifan1.zhang@amd.com>
2020-03-13 15:47:56 +0800

    Revert "drm/amdgpu: fix warning in ras_debugfs_create_all()"
    This reverts commit 1fb5b5a92c6a3f522d1ae0b8715259af4aa6b130.
    
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ras.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ras.c
@@ -1120,7 +1120,7 @@ void amdgpu_ras_debugfs_create(struct amdgpu_device *adev,
 void amdgpu_ras_debugfs_create_all(struct amdgpu_device *adev)
 {
 	struct amdgpu_ras *con = amdgpu_ras_get_context(adev);
-	struct ras_manager *obj;
+	struct ras_manager *obj, *tmp;
 	struct ras_fs_if fs_info;
 
 	/*
@@ -1132,7 +1132,10 @@ void amdgpu_ras_debugfs_create_all(struct amdgpu_device *adev)
 
 	amdgpu_ras_debugfs_create_ctrl_node(adev);
 
-	list_for_each_entry(obj, &con->head, node) {
+	list_for_each_entry_safe(obj, tmp, &con->head, node) {
+		if (!obj)
+			continue;
+
 		if (amdgpu_ras_is_supported(adev, obj->head.block) &&
 			(obj->attr_inuse == 1)) {
 			sprintf(fs_info.debugfs_name, "%s_err_inject",
