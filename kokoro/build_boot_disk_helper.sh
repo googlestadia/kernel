f0d28910474b3c26be0bd72e5938ce5fe594839e
Jean-François Roy <jfroy@google.com>
2020-08-04 19:10:57 -0700

    google: Add support for UEFI boot
    This patch adds support for UEFI boot using syslinux as the boot loader.
    UEFI boot is ultimately going to solve a lot of silly BIOS legacy
    problems, particularly as it pertains to PCIe BARs.
    
    - The boot disk now uses GPT.
    - The EFI system partition contains /boot with the same content as
      before (kernel image, initrd, etc).
    - The boot disk includes a BIOS boot compatibility partition.
    - The MBR's first 440 bytes are overwritten with syslinux's GPT MBR.
    
    This patch changes to sgdisk for partitioning, as it offers a slightly
    better interface than parted.
    
    See the following references for more information:
    - https://wiki.gentoo.org/wiki/Syslinux#Configuration
    - https://wiki.archlinux.org/index.php/Syslinux
    - https://wiki.archlinux.org/index.php/EFISTUB
    - https://wiki.archlinux.org/index.php/systemd-boot
    - http://www.rodsbooks.com/gdisk/booting.html
    - https://systemd.io/BOOT_LOADER_SPECIFICATION/
    - https://lists.gnu.org/archive/html/qemu-discuss/2018-04/msg00045.html
    
    Testing:
    - Build kernel image.
    
    - BIOS boot:
    $ qemu-system-x86_64 -machine q35 -m 512 -enable-kvm -nographic \
        -bios /media/nvme/bios_182.bin \
        -drive file=<disk.raw>,format=raw,if=virtio,id=boot,readonly \
        -net none
    
    - UEFI boot:
    $ cp /usr/share/OVMF/OVMF_VARS.fd ./
    $ qemu-system-x86_64 -machine q35 -m 512 -enable-kvm -nographic \
        -drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE.fd \
        -drive if=pflash,format=raw,file=./OVMF_VARS.fd \
        -drive file=<disk.raw>,format=raw,if=virtio,id=boot,readonly \
        -net none \
        -debugcon file:ovmf.log -global isa-debugcon.iobase=0x402
    
    - UEFI boot (nvme):
    $ cp /usr/share/OVMF/OVMF_VARS.fd ./
    $ qemu-system-x86_64 -machine q35 -m 512 -enable-kvm -nographic \
        -drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE.fd \
        -drive if=pflash,format=raw,file=./OVMF_VARS.fd \
        -drive file=<disk.raw>,format=raw,if=none,id=boot,readonly \
        -device nvme,drive=boot,serial=1,bootindex=0 \
        -net none \
        -debugcon file:ovmf.log -global isa-debugcon.iobase=0x402
    
    Bug: 153885920
    Change-Id: Iebe2d1adf970b392695c6a2e82cb2ebb16b83ea9

--- kokoro/build_boot_disk_helper.sh
+++ kokoro/build_boot_disk_helper.sh
@@ -16,7 +16,7 @@
 set -xe
 
 readonly BOOT_DISK="$1"
-readonly BOOT_DISK_MOUNT_DIR="$2"
+readonly MOUNTS_DIR="$2"
 readonly TAR_INSTALL_DIR="$3"
 readonly VMLINUZ_NAME="$4"
 readonly INITRD_NAME="$5"
@@ -26,18 +26,57 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-umount "${BOOT_DISK_MOUNT_DIR}" || true
-mkdir -p "${BOOT_DISK_MOUNT_DIR}"
-mount -o loop,offset=1048576 "${BOOT_DISK}" "${BOOT_DISK_MOUNT_DIR}"
-rm -rf "${BOOT_DISK_MOUNT_DIR}"/lost+found/
-rsync -a "${TAR_INSTALL_DIR}"/ "${BOOT_DISK_MOUNT_DIR}"/
-mkdir -p "${BOOT_DISK_MOUNT_DIR}"/boot/syslinux
-extlinux --install "${BOOT_DISK_MOUNT_DIR}"/boot/syslinux
-echo "default stadia
-timeout 0
-label stadia
-kernel /boot/${VMLINUZ_NAME}
-append initrd=/boot/${INITRD_NAME} root=auto ro console=ttyS0
-" > "${BOOT_DISK_MOUNT_DIR}"/boot/syslinux/syslinux.cfg
-chown -R root:root "${BOOT_DISK_MOUNT_DIR}" || true
-umount "${BOOT_DISK_MOUNT_DIR}"
+cleanup() {
+  umount /dev/mapper/loop0p1 || true
+  kpartx -vd "${BOOT_DISK}" || true
+  dmsetup remove loop0p1 || true
+  losetup -d /dev/loop0 || true
+}
+trap 'cleanup' ERR
+
+rm -f "${BOOT_DISK}"
+touch "${BOOT_DISK}"
+fallocate --zero-range --length 36M "${BOOT_DISK}"
+
+sgdisk \
+  --clear \
+  --new=1:: --typecode=1:EF00 --change-name=1:GGPBOOTFS \
+  --attributes=1:set:2 \
+  "${BOOT_DISK}"
+
+kpartx -va "${BOOT_DISK}"
+dmsetup --noudevsync mknodes
+
+/sbin/mkfs.vfat -s 1 -F 32 -n GGPBOOTFS /dev/mapper/loop0p1
+readonly EFI_MOUNT_DIR="${MOUNTS_DIR}"/efi
+mkdir -p "${EFI_MOUNT_DIR}"
+mount /dev/mapper/loop0p1 "${EFI_MOUNT_DIR}"
+
+rsync -a "${TAR_INSTALL_DIR}"/ "${EFI_MOUNT_DIR}"/
+
+mkdir -p "${EFI_MOUNT_DIR}"/boot/syslinux
+echo "SERIAL 0
+CONSOLE 0
+DEFAULT stadia
+TIMEOUT 0
+LABEL stadia
+  KERNEL /boot/${VMLINUZ_NAME}
+  APPEND initrd=/boot/${INITRD_NAME} root=auto ro console=ttyS0
+" > "${EFI_MOUNT_DIR}"/boot/syslinux/syslinux.cfg
+extlinux --install "${EFI_MOUNT_DIR}"/boot/syslinux
+
+mkdir -p "${EFI_MOUNT_DIR}"/EFI/BOOT
+cp /usr/lib/SYSLINUX.EFI/efi64/syslinux.efi \
+  "${EFI_MOUNT_DIR}"/EFI/BOOT/BOOTX64.EFI
+cp /usr/lib/syslinux/modules/efi64/ldlinux.e64 \
+  "${EFI_MOUNT_DIR}"/boot/syslinux/syslinux.cfg \
+  "${EFI_MOUNT_DIR}"/EFI/BOOT/
+
+# Copy the "boot sector" to its backup for fsck.fat, after unmounting.
+umount /dev/mapper/loop0p1
+dd if=/dev/mapper/loop0p1 of=/dev/mapper/loop0p1 bs=512 count=1 seek=6
+
+# Install syslinux's GPT bootloader code in the image's protective MBR.
+dd if=/usr/lib/syslinux/mbr/gptmbr.bin of=/dev/loop0
+
+cleanup
