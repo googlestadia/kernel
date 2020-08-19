62a8e18632fdcd662113e656b1c3bf8f93a43fce
Tiecheng Zhou <Tiecheng.Zhou@amd.com>
2020-08-19 10:27:09 +0800

    SWDEV-247673 - stop data_exchange work thread before reset
    In FLR routine, init_data_exchange is called at reset_sriov
    while fini_data_exchange is not. This will duplicating work
    thread.
    
    So call fini_data_exchange before reset for SRIOV
    
    Signed-off-by: Tiecheng Zhou <Tiecheng.Zhou@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -4021,9 +4021,12 @@ static int amdgpu_device_pre_asic_reset(struct amdgpu_device *adev,
 	int i, r = 0;
 	bool need_full_reset  = *need_full_reset_arg;
 
-	if (amdgpu_sriov_vf(adev))
+	if (amdgpu_sriov_vf(adev)) {
 		if (!amdgpu_virt_notify_booked(adev, job))
 			amdgpu_virt_wait_dump(adev, MAX_SCHEDULE_TIMEOUT);
+		/* stop the data exchange thread */
+		amdgpu_virt_fini_data_exchange(adev);
+	}
 
 	/* block all schedulers and reset given job's ring */
 	for (i = 0; i < AMDGPU_MAX_RINGS; ++i) {
